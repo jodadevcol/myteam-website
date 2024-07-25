5df914a0f9ec059399204538d38edff070f6e805
jodadev.col <jodadev.col@gmail.com>
2024-07-25 16:23:48 +1000

    chore Add content for intro in index page and const of color them project

---
+++ src/const.ts
@@ -0,0 +1,9 @@
+export const COLORS: { [key: string]: string } = {
+  MIDNIGHTGREEN: "--p-midnight-green",
+  LIGHTCORAL: "--p-light-coral",
+  RAPTUREBLUE: "--s-rapture-blue",
+  POLICEBLUE: "--s-police-blue",
+  DEEPJUNGLEGREEN: "--s-deep-jungle-green",
+  ACRAMENTOSTATEGREEN: "--s-acramento-state-green",
+  DARKGREEN: "--s-dark-green",
+}

dd3fec6e9676599124fba9ecb88f861531b99e96
jodadev.col <jodadev.col@gmail.com>
2024-07-25 16:22:29 +1000

    chore: updated colors theme and base styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,13 +4,13 @@ export default {
 	theme: {
 		extend: {
 			colors: {
-				"p-midnight-green": "#014e56",
-				"p-light-coral": "#f67e7e",
-				"s-rapture-blue": "#79c8c7",
-				"s-police-blue": "#2c6269",
-				"s-deep-jungle-green": "#004047",
-				"s-acramento-state-green": "#012f34",
-				"s-dark-green": "#002529",
+				"p-midnight-green": "var(--p-midnight-green)",
+				"p-light-coral": "var(--p-light-coral)",
+				"s-rapture-blue": "var(--s-rapture-blue)",
+				"s-police-blue": "var(--s-police-blue)",
+				"s-deep-jungle-green": "var(--s-deep-jungle-green)",
+				"s-acramento-state-green ": "var(--s-acramento-state-green )",
+				"s-dark-green": "var(--s-dark-green)",
 			}
 		},
 	},
